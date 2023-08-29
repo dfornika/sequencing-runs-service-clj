00d9e8a25753b6b1b6fbb28a82a523e07cdf8e95
Dan Fornika <dfornika@gmail.com>
2023-08-29 14:59:43 -0700

    Add keycloak realm loading script

---
+++ .github/scripts/load_keycloak_realm.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import requests
+
+
+def get_access_token(url, username='admin', password='admin'):
+    access_token = None
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    payload = {
+        'grant_type': 'password',
+        'client_id': 'admin-cli',
+        'username': username,
+        'password': password,
+    }
+    token_response = requests.post(args.base_url + 'realms/master/protocol/openid-connect/token', headers=headers, data=payload)
+    if token_response.status_code == requests.codes.ok:
+        access_token = token_response.json()['access_token']
+
+    return access_token
+
+
+def main(args):
+    realm = None
+    with open(args.realm_json, 'r') as f:
+        realm = json.load(f)
+
+    if not realm:
+        exit(-1)
+
+    access_token = get_access_token(args.base_url + 'realms/master/protocol/openid-connect/token')
+    if access_token:
+        headers = {
+            'Authorization': 'Bearer ' + access_token,
+            'Content-Type': 'application/json',
+        }
+        realms_response = requests.post(args.base_url + 'admin/realms', headers=headers, data=json.dumps(realm))
+        print(json.dumps(realms_response.json(), indent=2))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base-url', default="http://localhost:8080/")
+    parser.add_argument('--username', default="admin")
+    parser.add_argument('--password', default="admin")
+    parser.add_argument('--realm-json', required=True)
+    args = parser.parse_args()
+    main(args)
