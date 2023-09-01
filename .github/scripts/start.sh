94b32a942ff11adeb6147e3d61950d4ed1ed2eaf
Dan Fornika <dfornika@gmail.com>
2023-09-01 08:25:37 -0700

    Updage GraalVM workflow

--- .github/scripts/start.sh
+++ .github/scripts/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash 
 
-java -Xss256k -Xmx512m -XX:MaxRAM=100m -XX:+UseSerialGC -jar target/sequencing-runs-service-0.1.0-standalone.jar -c .github/data/test-config.edn
+java -Xss256k -Xmx512m -XX:MaxRAM=100m -XX:+UseSerialGC -jar target/sequencing-runs-service-0.1.0-standalone.jar -c .github/config/test-config.edn
